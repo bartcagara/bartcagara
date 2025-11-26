aa7e59a7be1221213bb1339b65c18472d7a3a057
Bartosz Cagara <bartcagara@Bartoszs-MacBook-Air.local>
2025-11-26 17:32:05 +0100

    Standardize global spacing: py-20 sections, mb-16 headers

--- src/components/home/HomePageClient.tsx
+++ src/components/home/HomePageClient.tsx
@@ -38,7 +38,7 @@ export default function HomePageClient() {
       </section>
 
       {/* TRUSTED BY SECTION */}
-      <section className={styles.bgFumee} style={{ paddingTop: '2rem', paddingBottom: '4rem', borderTop: '1px solid var(--grey-light)', borderBottom: '1px solid var(--grey-light)' }}>
+      <section className={`${styles.sectionPadding} ${styles.bgFumee}`} style={{ borderTop: '1px solid var(--grey-light)', borderBottom: '1px solid var(--grey-light)' }}>
         <div className={styles.container}>
           <div className={styles.trustLogos}>
             <p className={styles.trustLogosTitle}>Trusted by executives from:</p>
