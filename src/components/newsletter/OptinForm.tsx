224a9b13fac729bf77ae5beadbb3839b013f7122
Bartosz Cagara <bartcagara@Bartoszs-MacBook-Air.local>
2025-12-28 15:29:36 +0100

    Split briefing opt-in text into two lines

--- src/components/newsletter/OptinForm.tsx
+++ src/components/newsletter/OptinForm.tsx
@@ -150,7 +150,8 @@ export function OptinForm() {
         </div>
 
         <p className="text-xs text-bleu-nuit/60 font-black uppercase tracking-tight text-center mt-6 selection:bg-bleu-accent selection:text-white">
-          Join 400+ Former-Athlete Executives. Read in 2 minutes.
+          Join 400+ Former-Athlete Executives.<br />
+          Read in 2 minutes.
         </p>
       </form>
     </div>
